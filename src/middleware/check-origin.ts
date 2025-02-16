378dd1dd74ec8178c3f267378ae521f7f4f4cb73
Macbook Pro <macbookpro@MacBook-Pro-de-Macbook.local>
2025-02-16 12:22:01 -0300

    adicionado middleware de autenticação -7

--- src/middleware/check-origin.ts
+++ src/middleware/check-origin.ts
@@ -2,8 +2,9 @@ export const checkOrigin = (req: any, res: any, next: any) => {
     const ip = req.ip;
     const hostname = req.hostname;
     const isLocalhost = ip === '::1' || ip === '127.0.0.1' || ip.includes('::ffff:127.0.0.1');
+    const isHostname = hostname.includes("zendy.acutistecnologia.com")
     console.log(ip, isLocalhost, hostname)
-    if (isLocalhost) {
+    if (isLocalhost || isHostname) {
         next();
     } else {
         res.status(403).json({ error: "Access denied." });
