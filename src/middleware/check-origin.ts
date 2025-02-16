ae3b5c127a2c0b4b298fee5337038e45e05e261f
Macbook Pro <macbookpro@MacBook-Pro-de-Macbook.local>
2025-02-16 12:05:26 -0300

    adicionado middleware de autenticação -4

--- src/middleware/check-origin.ts
+++ src/middleware/check-origin.ts
@@ -1,7 +1,7 @@
 export const checkOrigin = (req: any, res: any, next: any) => {
     const ip = req.ip;
     const isLocalhost = ip === '::1' || ip === '127.0.0.1' || ip.includes('::ffff:127.0.0.1');
-
+    console.log(ip, isLocalhost)
     if (isLocalhost) {
         next(); 
     } else {
